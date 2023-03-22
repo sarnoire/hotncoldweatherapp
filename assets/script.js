b634e4b8e2e2df4af47c7cd5f88f127f85274c60
Sarah <115049982+sarnoire@users.noreply.github.com>
2023-03-21 19:13:05 -0600

    Fixed some issues I was having with the JS/HTML

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,53 @@
-var APIKey = "12341234123412341234123412341234";
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+const APIKey = "12341234123412341234123412341234";
+const queryURL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 
-function searchCity() {
-    // code to retrieve input value from the search form
-    const searchForm = document.getElementById('search-form'); // get the search form element
-    const searchInput = document.getElementById('search-input'); // get the search input element
-    
-    searchForm.addEventListener('submit', function(event) {
-      event.preventDefault(); // prevent the form from submitting
-    
-    const searchTerm = searchInput.value; // get the value of the search input
-    });
+async function getWeatherData(lat, lon) {
+  const url = `${queryUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
 
-    fetch(queryURL) // code to call an API and retrieve weather data for the city
-    
-    
+// display weather data in the UI
+function displayWeatherData(data) {
+  // display the location name
+  const locationName = data.city.name;
+  const locationEl = document.querySelector('#location');
+  locationEl.textContent = locationName;
 
-    // code to update the HTML with the weather data
-    // code to add the city to the search history
-  }
+// display the weather data for the next 5 days
+const weatherData = data.list.slice(0, 5);
+const forecastEl = document.querySelector('#forecast');
+forecastEl.innerHTML = '';
+weatherData.forEach((weather) => {
+  const date = new Date(weather.dt_txt);
+  const temp = Math.round(weather.main.temp - 273.15);
+  const description = weather.weather[0].description;
 
-document.querySelector('form').addEventListener('submit', searchCity);
-
-
-const searchForm = document.getElementById('search-form'); // get the search form element
-const searchInput = document.getElementById('search-input'); // get the search input element
-
-searchForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // prevent the form from submitting
-
-const searchTerm = searchInput.value; // get the value of the search input
-
-  // do something with the search term, such as fetching weather data for the city
+  const weatherEl = document.createElement('div');
+  weatherEl.classList.add('weather');
+  weatherEl.innerHTML = `
+    <div class="date">${date.toDateString()}</div>
+    <div class="temperature">${temp} &deg;C</div>
+    <div class="description">${description}</div>
+  `;
+  forecastEl.appendChild(weatherEl);
 });
+}
 
+// get the user's location and display the weather data
+function getWeatherForUserLocation() {
+navigator.geolocation.getCurrentPosition(
+  async (position) => {
+    const { latitude, longitude } = position.coords;
+    const data = await getWeatherData(latitude, longitude);
+    displayWeatherData(data);
+  },
+  (error) => {
+    console.error(error);
+  }
+);
+}
+
+getWeatherForUserLocation();
